7d21b6341220d1e65b001232b9ca65664e381481
MaikroX <you@example.com>
2022-05-17 15:43:49 +0200

    add the Final Screen

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let questions = [{
         "question": "Wer hat HTML erfunden ?",
-        "answer_1": "Robbie Wieliams",
+        "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners Lee",
         "answer_4": "Justin Bieber",
@@ -56,21 +56,31 @@ let questions = [{
     },
 ];
 
+let rightQuestions = 0;
+let currentQuestion = 0;
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
     showQuestion();
 }
 
-let currentQuestion = 0;
+function refreshPage() {
+    window.location.reload();
+}
+
+function comingsoon() {
+    alert("This function coming soon...")
+}
 
 function showQuestion() {
-
     if (currentQuestion >= questions.length) {
+        document.getElementById('quiz-container').innerHTML = ``;
 
+        document.getElementById('quiz-container').innerHTML += scoreCardTemp();
+        document.getElementById('amount_Questions').innerHTML = questions.length;
+        document.getElementById('amount_Of_Right_Questions').innerHTML = rightQuestions;
     } else {
         let question = questions[currentQuestion];
-
         document.getElementById('question_number').innerHTML = currentQuestion + 1;
         document.getElementById('questiontext').innerHTML = question.question;
         document.getElementById('answer_1').innerHTML = question.answer_1;
@@ -87,6 +97,7 @@ function answer(selection) { /* variable selecetion (Name frei wählbar) - wird 
     let idOfRightAnswer = `answer_${question.right_answer}`;
     if (selectedQuestionNumber == question.right_answer) { /* Wenn übereinstimmung dann Richtig oder Falsch */
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
