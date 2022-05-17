3c2b60b7b79df8de0541c0598866678e7b62ee5e
MaikroX <you@example.com>
2022-05-17 15:47:21 +0200

    tidy up the data

---
+++ js/data.js
@@ -0,0 +1,57 @@
+let questions = [{
+        "question": "Wer hat HTML erfunden ?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer ist der König des Internets ?",
+        "answer_1": "Paul Berger",
+        "answer_2": "King Knossi",
+        "answer_3": "Peter Fox",
+        "answer_4": "Max Mustermann",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet HTML ausgeschrieben ?",
+        "answer_1": "Hypertext Makeup Language",
+        "answer_2": "Hypertext Markup Language",
+        "answer_3": "Hypertext Mask Language",
+        "answer_4": "Hypertext Mirror Language",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches soziale Netzwerk hat Elon Musk im April 2022 für 44 Milliarden gekauft ?",
+        "answer_1": "Instagram",
+        "answer_2": "Facebook",
+        "answer_3": "Tinder",
+        "answer_4": "Twitter",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie endet eine Domain die auf den Malediven registriert wird ?",
+        "answer_1": ".de",
+        "answer_2": ".com",
+        "answer_3": ".ma",
+        "answer_4": ".mv",
+        "right_answer": 4
+    },
+    {
+        "question": "Wer hat nach Instagram selbst die meisten Follower (423 Mio.) auf der Plattform Instagram - stand: 07.April 2022 ?",
+        "answer_1": "Christiano Ronaldo",
+        "answer_2": "Kylie Jenner",
+        "answer_3": "Kim Kardashian",
+        "answer_4": "The Rock",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie heißt der Gründer des Onlineversandhändlers Amazon ?",
+        "answer_1": "Steve Jobs",
+        "answer_2": "Jeff Bezos",
+        "answer_3": "Elon Musk",
+        "answer_4": "Jeve Sobs",
+        "right_answer": 4
+    },
+];
